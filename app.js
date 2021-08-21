41d95b72d371e01739240361e1f645803afa9286
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-08-21 13:23:06 +0800

    Set express-handlebars as view engine

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+
+// set handlebars as view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
